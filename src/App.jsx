d2918e22f951834d3bb6f9a656da7eb5c696280b
shaylashobnom <shaylashobnomankhi@gmail.com>
2024-05-20 00:51:13 +0600

    add backup data

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppRouter from "./components/routes/Router";
+
+function App() {
+  return (
+    <main>
+      <AppRouter />
+    </main>
+  );
+}
+
+export default App;
